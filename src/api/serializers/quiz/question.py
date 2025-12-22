3db8324d3b302f09a49a0d91d757556d0472bf13
Shukrullo <bekbek791@gmail.com>
2025-12-22 11:18:43 +0300

    nimadur

--- src/api/serializers/quiz/question.py
+++ src/api/serializers/quiz/question.py
@@ -6,3 +6,7 @@ class QuestionSerializer(BaseSerializer):
         model = QuestionModel
         fields = '__all__'
     
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['answers'] = AnswerSerializer(instance.answers, many=True).data
+        return rep
